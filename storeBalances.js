68ca002428bf17e2035c6fcc177e44339f462655
Ross Dyson <dyson.ross@gmail.com>
2017-12-27 00:00:18 +0800

    switch to async await

--- storeBalances.js
+++ storeBalances.js
@@ -4,20 +4,20 @@ const fetch = require('node-fetch');
 const summaryUrl = 'https://wt-07d3444cd5d8561f1c4063625863e880-0.sandbox.auth0-extend.com/webtask-bitfinex-data-dev-getFinexData?summary=true';
 const taskName = 'STORE_BALANCES';
 
-module.exports = (ctx, cb) => {
+module.exports = async function (ctx, cb) {
   console.log(`${taskName} NEW_STORE_BALANCES_REQUEST`);
   console.log(`${taskName} GETTING_WALLET_SUMMARY`);
-  fetch(summaryUrl).then(res => {
-    if(!res.ok) {
+  try {
+    let res = await fetch(summaryUrl);
+    if(res.ok) {
+      let data = await res.json();
+      cb(null, data);
+    } else {
       throw new Error(res);
     }
-    return res.json();
-  }).then(data => {
-    console.log(`${taskName} GOT_WALLET_SUMMARY`);
-    cb(null, data);
-  }).catch(err => {
+  } catch(err) {
     console.log(`${taskName} FETCH_ERROR:`);
     console.log(err);
     cb(null, err);
-  });
+  }
 };
